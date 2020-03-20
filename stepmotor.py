491304ea225197165aadc8afd4a8ac66143bbf36
Simon <ficstudio@yahoo.com.tw>
2020-03-20 10:58:08 +0800

    v0.0.9b - Add thread for stepper motor one step.

--- stepmotor.py
+++ stepmotor.py
@@ -1,8 +1,22 @@
+from adafruit_motorkit import MotorKit
+import threading
+
 # coordinates, left hand rule
 x = 0.0 # left +, right -
 y = 0.0 # forward +, back -
 z_a = 0.0 # down +, up -
 z_b = 0.0 # down +, up -
+
+class motor_move(threading.Thread):
+    def __init__(self, m, f, v): # m = kit.stepper (1)
+        super().__init__()
+        self.m = m
+        self.f = f
+        self.tenv = int(10*v)
+        
+    def run(self):
+        for _ in range(self.tenv):
+            self.m.onestep(direction=self.f)
 
 def left(value = 0.1):
     global x
