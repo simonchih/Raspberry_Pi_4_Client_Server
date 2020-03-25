08a87a52c8b514959d39428cb66dbf3839c6883b
Simon <ficstudio@yahoo.com.tw>
2020-03-25 10:50:01 +0800

    v0.0.9c: test code

--- stepmotor.py
+++ stepmotor.py
@@ -1,5 +1,7 @@
 from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
 import threading
+import time
 
 # coordinates, left hand rule
 x = 0.0 # left +, right -
@@ -15,26 +17,52 @@ class motor_move(threading.Thread):
         self.tenv = int(10*v)
         
     def run(self):
+        print('self.tenv = %d' % self.tenv) #temp
+        print('self.f = %s' % self.f) #temp
+        
+        #self.m.release()
+        
         for _ in range(self.tenv):
             self.m.onestep(direction=self.f)
+            time.sleep(0.01)
 
 def left(value = 0.1):
     global x
+    
+    kit = MotorKit()
+    th_x = motor_move(kit.stepper1, stepper.FORWARD, value)
+    th_x.start()
+    th_x.join()
     
     x += value
     
 def right(value = 0.1):
     global x
     
+    kit = MotorKit()
+    th_x = motor_move(kit.stepper1, stepper.BACKWARD, value)
+    th_x.start()
+    th_x.join()
+    
     x -= value
     
 def forward(value = 0.1):
     global y
     
+    kit = MotorKit()
+    th_y = motor_move(kit.stepper2, stepper.FORWARD, value)
+    th_y.start()
+    th_y.join()
+    
     y += value
     
 def back(value = 0.1):
     global y
+    
+    kit = MotorKit()
+    th_y = motor_move(kit.stepper2, stepper.BACKWARD, value)
+    th_y.start()
+    th_y.join()
     
     y -= value
     
