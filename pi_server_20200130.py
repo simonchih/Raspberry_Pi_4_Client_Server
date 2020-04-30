e2d7ed7a234cceb2f58840202ffb13c27fa2dcec
Simon <ficstudio@yahoo.com.tw>
2020-04-30 14:14:26 +0800

    v0.0.9f: marked GPIO related code as comment

--- pi_server_20200130.py
+++ pi_server_20200130.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import socket
 import time
 
@@ -77,20 +77,20 @@ def load_json(slot):
     return data
 
 # blinking function
-def blink(pin = 7):
-    # to use Raspberry Pi board pin numbers
-    GPIO.setmode(GPIO.BOARD)
-
-    # set up GPIO output channel, we set GPIO4 (Pin 7) to OUTPUT
-    GPIO.setup(pin, GPIO.OUT)
-    
-    GPIO.output(pin,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(pin,GPIO.LOW)
-    time.sleep(1)
-    
-    GPIO.cleanup()
-    return
+#def blink(pin = 7):
+#    # to use Raspberry Pi board pin numbers
+#    GPIO.setmode(GPIO.BOARD)
+#
+#    # set up GPIO output channel, we set GPIO4 (Pin 7) to OUTPUT
+#    GPIO.setup(pin, GPIO.OUT)
+#    
+#    GPIO.output(pin,GPIO.HIGH)
+#    time.sleep(1)
+#    GPIO.output(pin,GPIO.LOW)
+#    time.sleep(1)
+#    
+#    GPIO.cleanup()
+#    return
 
 def left(value = 0.1):
     
